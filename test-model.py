ee5ee0f964d6cc4cf47dc1a361b364faeab38d1e
shbacon <77406398+shbacon@users.noreply.github.com>
2022-07-12 10:08:13 -0400

    Update test-model.py

--- test-model.py
+++ test-model.py
@@ -2,11 +2,11 @@ from tflite_support.task import audio
 from tflite_support.task import core
 from tflite_support.task import processor
 
-base_options = core.BaseOptions(file_name='/Users/tbacon/Desktop/tensor/birds_models/my_birds_model.tflite')
+base_options = core.BaseOptions(file_name='/Users/shbacon/Desktop/tensor/birds_models/my_birds_model.tflite')
 classification_options = processor.ClassificationOptions(max_results=1)
 options = audio.AudioClassifierOptions(base_options=base_options, classification_options=classification_options)
 classifier = audio.AudioClassifier.create_from_options(options)
 
-audio_file = audio.TensorAudio.create_from_wav_file('/Users/tbacon/Desktop/tensor/dataset/small_birds_dataset/test/gun/gun1-1.wav', classifier.required_input_buffer_size)
+audio_file = audio.TensorAudio.create_from_wav_file('/Users/shbacon/Desktop/tensor/dataset/small_birds_dataset/test/gun/gunshots0_00002_00.wav', classifier.required_input_buffer_size)
 audio_result = classifier.classify(audio_file)
 print(audio_result)
